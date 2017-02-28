2c0ab30dc2fb3d4971fd1ec8a9e29385ce4c8e30
Federico Iachetti <iachetti.federico@gmail.com>
2017-02-28 08:45:11 -0300

    Creating readme

--- README.md
+++ README.md
@@ -1,5 +1,81 @@
 # README
 
+## Prerequisites
+* Rails 5
+* Ruby >=2.2
+* Foreman gem
+* NodeJS
+* Yarn
+
+## Instructions to install and configure prerequisites or dependencies, if any
+
+After downloading the zip file containing the project, unzip it on the desired directory.
+
+Then cd into the directory and run bundle and yarn
+```
+  cd ticket_master
+  bundle
+  cd client
+  yarn
+  cd ..
+```
+
+The package dependencies will be intalled at this point.
+
+Note that the application won't run yet because we first need to set up the database
+
+
+## Instructions to create and initialize the database (if required)
+In order to create and populate the database, we need to create a `config/secrets.yml` file inside our application directory.
+
+I've left an example file on the repository, so we start by copying it to it destination
+
+```
+  cp config/secrets.example.yml config/secrets.yml
+```
+
+Then we fill out the user and password for our MySQL database user.
+
+We also need to set an email and password for the first admin user of the system.
+
+We might end up with a file like this one:
+
+```yml
+  development:
+    secret_key_base: 9bf75221023397316e0ba90f36d463ffa0ef08aa185dba4d69de679f66df9d451aa8387dabe4756fed4551dbb081a27d6a3bf8dfd03b6bd8ba7cd5647106c8dc
+    db_user:     myself
+    db_password: 123456789
+    admin_mail:  "admin@example.com"
+    admin_pass:  "123456"
+  test:
+    secret_key_base: 13125c4ac42f46b67e4d9f3e3c59521fcd1d0b9ad1251cc10e01da3a928d105df54bd7a90cf10813f8e45995e3855e058db98787be2537630381eca5e287e1d1
+    db_user:     myself
+    db_password: 123456789
+  
+  production:
+    secret_key_base: <%= ENV["SECRET_KEY_BASE"] %>
+```
+
+Then we can create, migrate and seed the database
+
+```
+rails db:create
+rails db:migrate
+rails db:seed
+```
+
+Note that seeding the database is important. This step will create the default roles for the system and the admin user.
+
+# Assumptions you have made - it is good to explain your thought process and the assumptions you have made
+
+* Requirements that you have not covered in your submission, if any
+
+* Instructions to configure and prepare the source code to build and run properly
+
+* Issues you have faced while completing the assignment, if any
+
+* Constructive feedback for improving the assignment
+
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,39 @@
+puts "* Creating default roles"
 admin_role    = Role.create!(name: "admin")
 agent_role    = Role.create!(name: "agent")
 customer_role = Role.create!(name: "customer")
 
+puts "* Creating admin account"
 admin = Account.create!(email:    Rails.application.secrets[:admin_mail],
                         password: Rails.application.secrets[:admin_pass],
                         roles:    [admin_role])
 
-accounts = 1.upto(5).flat_map do |i|
-  pass = "123456"
-  [
-   Account.create!(name: "customer#{i}",
-                   email: "customer#{i}@example.com", password: pass,
-                   roles: [customer_role]),
-   Account.create!(name: "agent#{i}",
-                   email: "agent#{i}@example.com",    password: pass,
-                   roles: [agent_role]),
-   Account.create!(name: "agtcustr#{i}",
-                   email: "agentcustomer#{i}@example.com", password: pass,
-                   roles: [agent_role, customer_role])
-  ]
-end
+if Rails.env == "development"
+  accounts = 1.upto(5).flat_map do |i|
+    pass = "123456"
+    [
+     Account.create!(name: "customer#{i}",
+                     email: "customer#{i}@example.com", password: pass,
+                     roles: [customer_role]),
+     Account.create!(name: "agent#{i}",
+                     email: "agent#{i}@example.com",    password: pass,
+                     roles: [agent_role]),
+     Account.create!(name: "agtcustr#{i}",
+                     email: "agentcustomer#{i}@example.com", password: pass,
+                     roles: [agent_role, customer_role])
+    ]
+  end
 
+  1.upto(30) do |i| Ticket.create!(customer: Account.customers.sample, title:
+                                   "Ticket #{i}", description: "Description for Ticket #{i}") end
 
-1.upto(30) do |i| Ticket.create!(customer: Account.customers.sample, title:
-  "Ticket #{i}", description: "Description for Ticket #{i}") end
+  current_agent = Account.where(email: "agent1@example.com").first
+  (-60..60).each do |days|
+    Ticket.create!(title: "Closed ticket", description: "This is a closed ticket. ",
+                   workflow_state: "resolved",
+                   customer: Account.customers.sample, agent: current_agent,
+                   created_at: Date.today - days.days - rand(5),
+                   closed_on: Date.today - days.days)
+  end
 
-current_agent = Account.where(email: "agent1@example.com").first
-(-60..60).each do |days|
-  Ticket.create!(title: "Closed ticket", description: "This is a closed ticket. ",
-                 workflow_state: "resolved",
-                 customer: Account.customers.sample, agent: current_agent,
-                 created_at: Date.today - days.days - rand(5),
-                 closed_on: Date.today - days.days)
 end
